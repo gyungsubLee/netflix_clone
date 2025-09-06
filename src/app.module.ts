78d8f4d58cfc10ee4fb05c381384bd3b897a330b
gyoungsubLee <dltjq2323@gmail.com>
2025-09-06 16:13:25 +0900

    feat: DB 설정 ( PostgreSQL )

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,32 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // .env 로딩
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (cfg: ConfigService) => ({
+        type: 'postgres',
+        host: cfg.get<string>('DB_HOST'),
+        port: cfg.get<number>('DB_PORT'),
+        username: cfg.get<string>('DB_USERNAME'),
+        password: cfg.get<string>('DB_PASSWORD'),
+        database: cfg.get<string>('DB_DATABASE'),
+        entities: [User],
+        synchronize: true, // 운영에서는 반드시 false
+        logging: true,
+        namingStrategy: new SnakeNamingStrategy(),
+      }),
+    }),
+    TypeOrmModule.forFeature([User]),
+    AuthModule,
+    UserModule,
+  ],
+})
 export class AppModule {}
